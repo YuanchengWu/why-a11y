06ea97de7d9e6596eec0e0451925f1c95156b0f3
John Wu <johnwu0202@gmail.com>
2021-05-21 11:31:43 -0700

    Add secret and change manifest

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import "./App.css";
 import "@workday/canvas-kit-css-button/index.scss";
 import { useState } from "react";
+import { useKonami } from "react-konami-code";
 
 import { Form } from "./Form";
 
 export default function App(): JSX.Element {
   const [showPage, setShowPage] = useState(false);
   const [start, setStart] = useState(false);
+  useKonami(handleUnveil);
 
   function handleStartClick() {
     setStart(true);
   }
 
   function handleUnveil() {
+    setStart(true);
     setShowPage(true);
+  }
+
+  function handleReset() {
+    setStart(false);
+    setShowPage(false);
   }
 
   return (
@@ -29,20 +37,16 @@ export default function App(): JSX.Element {
           </button>
         </div>
       </div>
-      <div id="veil" className={`${showPage && "moved"}`} aria-hidden>
-        <div>
-          <button
-            className="wdc-btn wdc-btn-size-s developer-btn"
-            onClick={handleUnveil}
-            tabIndex={-1}
-          >
-            I am the developer (unhide)
-          </button>
-        </div>
-      </div>
+      <div id="veil" className={`${showPage && "moved"}`} aria-hidden />
       <div className="centered background">
         <Form />
       </div>
+      <button
+        className="wdc-btn wdc-btn-size-s reset-btn"
+        onClick={handleReset}
+      >
+        Reset
+      </button>
     </>
   );
 }
